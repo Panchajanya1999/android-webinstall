23a2028c32463ce1427a41f0e0f1538803399911
Panchajanya1999 <rsk52959@gmail.com>
2023-05-08 01:50:01 +0530

    Barebone adaptation for azure kernel
    Signed-off-by: Panchajanya1999 <rsk52959@gmail.com>

--- src/config.js
+++ src/config.js
@@ -1,66 +1,29 @@
 export default {
     OS_NAME: process.env.VUE_APP_OS_NAME,
-    ACCENT_COLOR: "#0366d6",
+    ACCENT_COLOR: "#0a293d",
     SUPPORTED_DEVICES: [
         {
-            name: "Pixel 4",
-            model: "flame",
+            name: "Pixel 6/Pro",
+            model: "raviole",
         },
         {
-            name: "Pixel 4 XL",
-            model: "coral",
-        },
-        {
-            name: "Pixel 4a 5G",
-            model: "bramble",
-        },
-        {
-            name: "Pixel 5",
-            model: "redfin",
-        },
-        {
-            name: "Pixel 6",
-            model: "oriole",
-        },
-        {
-            name: "Pixel 6 Pro",
-            model: "raven",
+            name: "Pixel 6A",
+            model: "bluejay",
         },
     ],
     RELEASE_VARIANTS: {
-        minimal: {
-            description: "Minimal version without Google apps or services.",
-            suffix: "",
-        },
-        gapps: {
-            description:
-                "Version with Google services included, so the apps you’re familiar with will work. Recommended for most users.",
-            suffix: " with Google services",
-        },
-        test: {
-            description: "Experimental version for testing only.",
-            suffix: "experimental",
-        },
-        beta: {
-            description: "Beta version for testing only.",
-            suffix: "beta",
-        },
+	stable: {
+		description: "Stable Kernel Releases",
+		suffix: "",
+	},
     },
     DONATION_LINKS: [
-        {
-            title: "Recurring donation",
-            description:
-                "Please consider making a recurring donation on Patreon for benefits such as early access to updates, exclusive behind-the-scenes development news, and priority support. This allows us to support the project sustainably in the future.",
-            highlight: true,
-            url: "https://patreon.com/kdrag0n",
-            icon: "patreon",
-        },
         {
             title: "One-time donation",
             description:
                 "You can also make a one-time donation through PayPal. It doesn’t help with sustainable support, but every little bit is appreciated!",
             highlight: false,
-            url: "https://paypal.me/kdrag0ndonate",
+            url: "https://paypal.me/panchajanya1999",
             icon: "paypal",
         },
     ],
